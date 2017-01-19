5031342a427a6d0d2ad4561dfbd7cecbf1e02eda
Justin Peck <jdpeck90@gmail.com>
2017-01-19 11:06:26 -0500

    Lesson 58

--- Tutorials/reactnative/Udemy/auththree/src/App.js
+++ Tutorials/reactnative/Udemy/auththree/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { View, Text } from 'react-native';
 import { Header } from './components/common/Index';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyDra7tu4MDeWIv-ut1173VmIC8ZxGB-e5g',
+    authDomain: 'auth-reactnative-29a96.firebaseapp.com',
+    databaseURL: 'https://auth-reactnative-29a96.firebaseio.com',
+    storageBucket: 'auth-reactnative-29a96.appspot.com',
+    messagingSenderId: '398858069094'
+  });
+  }
+
   render() {
     return (
     <View>
