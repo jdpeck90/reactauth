3b648bff22d380d7fdcf1584424ac000f8227ee9
Justin Peck <jdpeck90@gmail.com>
2017-01-19 13:35:15 -0500

    Lesson 73

--- Tutorials/reactnative/Udemy/auththree/src/components/common/Spinner.js
+++ Tutorials/reactnative/Udemy/auththree/src/components/common/Spinner.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
-const Spinner = () => {
-  return (
-    <View />
+
+const Spinner = ({ size }) => (
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
     );
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 };
 
 export { Spinner };
